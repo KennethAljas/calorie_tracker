6cef147cbfb8d9bd87c79a7fe8457131a3a054c3
Alkem <mynamezalter@gmail.com>
2025-10-17 20:48:08 +0300

    Add checkmark, fix input styling

--- script.js
+++ script.js
@@ -152,9 +152,20 @@ closeTaskView.addEventListener('click', function () {
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const task = document.createElement("input")
-    task.className = "task"
-    task.value = taskInput.value
+    const task = document.createElement("div")
+    task.className = "taskItem"
+
+    task.innerHTML = `
+    <input class="task" value="${taskInput.value}">
+    <label class="container">
+        <input type="checkbox">
+        <svg viewBox="0 0 64 64" height="2em" width="2em">
+        <path d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16" 
+                pathLength="575.0541381835938" class="path"></path>
+        </svg>
+    </label>
+    `
+
     taskParent.appendChild(task)
 
     blurScreen.style.display = "none"
